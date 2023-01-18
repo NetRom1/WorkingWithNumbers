21c6e3396b043e5866ab9f91845243c3230d5d80
NetRo <ramulrimov3@gmail.com>
2023-01-18 21:40:47 +0300

    Corrected the output

--- src/StreamMain.java
+++ src/StreamMain.java
@@ -2,16 +2,17 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamMain {
     public static void main(String[] args) {
-        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4)
-                .stream()
-                .filter(integer -> integer > 0)
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Stream<Integer> stream = intList.stream();
+        stream.filter(integer -> integer > 0)
                 .filter(integer -> integer%2 == 0)
                 .sorted(Comparator.naturalOrder())
-                .collect(Collectors.toList());
-        System.out.println(intList);
-
+                .map(integer -> integer + " ")
+                .collect(Collectors.toList())
+                .forEach(System.out::print);
     }
 }
